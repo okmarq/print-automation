ab9db0f3641a5ea433d1a69a00c795d811a79cc1
Joel Okoromi <okmarq@gmail.com>
2024-10-14 10:33:23 +0100

    setup migrations

---
+++ database/migrations/2024_10_11_181009_create_admin_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('version')->unique();
+            $table->decimal('cost_bw_page')->default(0);
+            $table->decimal('cost_color_page')->default(0);
+            $table->decimal('cost_pixel_image', 10, 8)->default(0);
+            $table->boolean('is_preferred')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_settings');
+    }
+};
---
+++ database/migrations/2024_10_11_181028_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('print_job_id')->references('id')->on('print_jobs')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->decimal('amount')->default(0);
+            $table->string('status')->default('unpaid');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
