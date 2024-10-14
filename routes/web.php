6b2643c96c698989c1a878d462d2b03ee5259514
Joel Okoromi <okmarq@gmail.com>
2024-10-14 10:29:21 +0100

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminSettingController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PrintJobController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,14 +10,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('dashboard', function () {
+    return (new PrintJobController)->index();
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [AdminSettingController::class, 'index'])->name('settings.index');
+        Route::get('settings', [AdminSettingController::class, 'create'])->name('settings.create');
+        Route::post('settings', [AdminSettingController::class, 'store'])->name('settings.store');
+        Route::patch('settings/{adminSetting}', [AdminSettingController::class, 'store'])->name('settings.update');
+    });
+
+    Route::prefix('print')->group(function () {
+        Route::get('/', [PrintJobController::class, 'index'])->name('print.upload');
+        Route::get('upload', [PrintJobController::class, 'create'])->name('print.create');
+        Route::post('upload', [PrintJobController::class, 'store'])->name('print.upload.store');
+        Route::post('submit', [PrintJobController::class, 'submit']);
+    });
+    Route::post('pay/{printJob}', [PaymentController::class, 'store'])->name('payment.store');
 });
 
 require __DIR__.'/auth.php';
