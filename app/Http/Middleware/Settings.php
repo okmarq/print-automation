a1a4c3df1d28c7988f8106f3601f08b90c91ba9e
Joel Okoromi <okmarq@gmail.com>
2024-10-15 20:35:55 +0100

    perform cleanup

--- app/Http/Middleware/Settings.php
+++ app/Http/Middleware/Settings.php
@@ -14,7 +14,7 @@ class Settings
     /**
      * Handle an incoming request.
      *
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param \Closure(Request): (Response) $next
      * @throws Exception
      */
     public function handle(Request $request, Closure $next): Response
