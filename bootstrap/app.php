dffaf8725b0b044930046de3aeba6a2f36715242
Joel Okoromi <okmarq@gmail.com>
2024-10-15 22:53:24 +0100

    perform cleanup for deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(Settings::class);
+        $middleware->alias([
+            'settings' => Settings::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
