50c2cb76c354805fc25badfb78f01eeefc544fbe
Joel Okoromi <okmarq@gmail.com>
2024-10-15 21:28:28 +0100

    perform cleanup for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -17,5 +17,8 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
 
+echo "Link storage"
+php artisan storage:link
+
 echo "Running Queue"
 php artisan queue:work
