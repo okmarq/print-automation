db0471fd22e7252fa25a52d024bca7c560df8270
Joel Okoromi <okmarq@gmail.com>
2024-10-15 17:34:29 +0100

    update pages

--- database/migrations/2024_10_11_181019_create_print_jobs_table.php
+++ database/migrations/2024_10_11_181019_create_print_jobs_table.php
@@ -7,31 +7,32 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-  /**
-   * Run the migrations.
-   */
-  public function up(): void
-  {
-    Schema::create('print_jobs', function (Blueprint $table) {
-      $table->id();
-      $table->string('file_path');
-      $table->integer('total_pages')->default(0);
-      $table->integer('black_white_pages')->default(0);
-      $table->integer('colored_pages')->default(0);
-      $table->integer('total_images')->default(0);
-      $table->bigInteger('total_pixels')->default(0);
-      $table->decimal('amount')->default(0);
-      $table->foreignId('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-      $table->softDeletes();
-      $table->timestamps();
-    });
-  }
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('print_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_path');
+            $table->integer('total_pages')->default(0);
+            $table->integer('black_white_pages')->default(0);
+            $table->integer('colored_pages')->default(0);
+            $table->integer('total_images')->default(0);
+            $table->bigInteger('total_pixels')->default(0);
+            $table->decimal('amount')->default(0);
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('status')->default('unpaid');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void
-  {
-    Schema::dropIfExists('print_jobs');
-  }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('print_jobs');
+    }
 };
